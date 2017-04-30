6cae992c2746ce0091426a7e3df4c81109505574
Djair Carvalho <djaircarvalho.dj7@gmail.com>
2017-04-30 09:32:32 -0300

    modificado logradouro p liestring

--- db/migrate/20170426173621_create_logradouros.rb
+++ db/migrate/20170426173621_create_logradouros.rb
@@ -2,7 +2,7 @@ class CreateLogradouros < ActiveRecord::Migration[5.0]
   def change
     create_table :logradouros do |t|
       t.string :descricao
-      t.st_polygon :localizacao, :geographic => true, :srid => 4055
+      t.line_string :localizacao, :geographic => true, :srid =>4055 
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -43,9 +43,9 @@ ActiveRecord::Schema.define(version: 20170428200857) do
 
   create_table "logradouros", force: :cascade do |t|
     t.string    "descricao"
-    t.geography "localizacao", limit: {:srid=>4055, :type=>"polygon", :geographic=>true}
-    t.datetime  "created_at",                                                             null: false
-    t.datetime  "updated_at",                                                             null: false
+    t.geography "localizacao", limit: {:srid=>4055, :type=>"line_string", :geographic=>true}
+    t.datetime  "created_at",                                                                 null: false
+    t.datetime  "updated_at",                                                                 null: false
   end
 
   create_table "pontes", force: :cascade do |t|
