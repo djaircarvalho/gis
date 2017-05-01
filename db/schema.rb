5779c545c36863e1112e56fd34652a0e2d1ac474
Catharina Mesquita <catha.ana.1994@gmail.com>
2017-05-01 10:43:14 -0300

    ajuste na entidade rodovia

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170501131311) do
+ActiveRecord::Schema.define(version: 20170501133259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(version: 20170501131311) do
     t.geometry "geom",       limit: {:srid=>0, :type=>"line_string"}
     t.datetime "created_at",                                          null: false
     t.datetime "updated_at",                                          null: false
+    t.string   "descricao"
   end
 
 end
